843feaef05bba6944e823486f26784d1c3c62b1e
Jordan <jordmax12@gmail.com>
2021-01-23 15:51:53 -0600

    pre-lint

--- application/v1/controller/console/dbversioner.js
+++ application/v1/controller/console/dbversioner.js
@@ -1,28 +1,30 @@
 const path = require('path');
 const fs = require('fs');
 const sorter = require('./common/sorter');
-const { getConnection, query } = require('../../model/postgres');
-require('dotenv').config()
+const {getConnection, query} = require('../../model/postgres');
+require('dotenv').config();
 
-const VERSIONS_DIRECTORY = 'sql';
+const VERSIONS_DIRECTORY = 'migrations';
+const STAR_COUNT = 80;
 
 function processSQLFile(fileName) {
     // Extract SQL queries from files. Assumes no ';' in the fileNames
-    const queries = fs.readFileSync(fileName).toString()
-      .replace(/(\r\n|\n|\r)/gm," ") // remove newlines
-      .replace(/\s+/g, ' ') // excess white space
-      .split(";") // split into all statements
-      .map(Function.prototype.call, String.prototype.trim)
-      .filter(function(el) {return el.length != 0}); // remove any empty ones
+    const queries = fs
+        .readFileSync(fileName).toString()
+        .replace(/(\r\n|\n|\r)/gm,' ') // remove newlines
+        .replace(/\s+/g, ' ') // excess white space
+        .split(';') // split into all statements
+        .map(Function.prototype.call, String.prototype.trim)
+        .filter(el => {return el.length !== 0}); // remove any empty ones
 
     return queries;
 }
 
+const _get_stars = () => {
+    return '*'.repeat(STAR_COUNT);
+}
+
 module.exports.applyVersion = async () => {
-    // return new Promise((resolve) => {
-        console.log('here')
-        // resolve()
-    // })
     const dir = path.join(process.cwd(), VERSIONS_DIRECTORY);
     const files = sorter.sortFiles(await fs.readdirSync(dir), '.sql');
     const db_connection = getConnection({
@@ -31,17 +33,20 @@ module.exports.applyVersion = async () => {
         db: process.env.POSTGRES_DB,
         port: process.env.POSTGRES_PORT,
         host: process.env.POSTGRES_HOST
-    })
-    console.log('logging db_connection', db_connection)
-    for(const file of files) {
+    });
+
+    let total_query_count = 0;
+
+    for(const [index, file] of files.entries()) {
         const queries = processSQLFile(`${dir}/${file}`);
-        
+        console.log('\n', _get_stars(), `\n\n\tStarting Queries on migration ${index + 1}...\n\n`, _get_stars());
         for(const _query of queries) {
-            console.log(`EXECUTING QUERY: ${_query}`);
+            console.log(`\nEXECUTING QUERY: ${_query}`);
             await query(db_connection, _query);
-            console.log('FINISHED QUERY')
+            total_query_count++;
+            console.log('FINISHED QUERY\n');
         }
     }
 
-    console.log("DB migrations successful");
+    console.log('\n', _get_stars(), `\n\n\tDB migrations successful, total migrations: ${files.length}, total operations: ${total_query_count}\n\n`, _get_stars());
 }
--- application/v1/controller/server.js
+++ application/v1/controller/server.js
@@ -10,17 +10,17 @@ app.use(
   bodyParser.urlencoded({
     extended: false
   })
-)
-app.use(cors())
-app.use(bodyParser.json())
-const ROUTES_PATH = 'application/v1/controller/routes'
+);
+app.use(cors());
+app.use(bodyParser.json());
+const ROUTES_PATH = 'application/v1/controller/routes';
 const routes = path.join(process.cwd(), ROUTES_PATH);
 glob.sync(`${routes}/*.js`).forEach(function(file) {
-  require(path.resolve(file))(app)
+  require(path.resolve(file))(app);
 })
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
-})
+    console.log('listening on port 3000');
+});
 
-module.exports = app
+module.exports = app;
