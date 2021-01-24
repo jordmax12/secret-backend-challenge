65cd7d760eff2f43d37bbf47b56a2528328f6f46
Jordan <jordmax12@gmail.com>
2021-01-23 20:52:19 -0600

    got rid of console log, added todo

--- application/v1/controller/routes/plan.js
+++ application/v1/controller/routes/plan.js
@@ -2,7 +2,7 @@ const { getPlan } = require('../../logic/factories/plan');
 
 module.exports = (app) => {
     app.get('/v1/plan/', async (request, response) => {
-        console.log('logging params', request.query)
+        // TODO: need to add validator to make sure we have rollLength + rollLength is an float
         const results = await getPlan(request.query.rollLength);
         response.status(200).send({
             data: results
